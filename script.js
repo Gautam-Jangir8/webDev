e54d8678f0c026c0464b573884e716d45d96c938
Gautam Jangir <83230112+Gautam-Jangir8@users.noreply.github.com>
2022-01-09 11:49:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+<script src='https://kit.fontawesome.com/a076d05399.js'></script>
+  <div class="container"id="container"data-index="0">
+    <button class="left"  onclick="next('left')"><i class='fas fa-angle-left'></i></button>
+    <button class="right" onclick="next('right')"><i class='fas fa-angle-right'></i></button>
+  </div>
